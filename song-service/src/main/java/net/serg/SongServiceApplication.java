f1058ce82d0925e71c569241311d74276de55c4b
Sergey Pil <realsergeypil@gmail.com>
2024-02-26 17:04:15 +0600

    Refactor to automatic build

--- song-service/src/main/java/net/serg/SongServiceApplication.java
+++ song-service/src/main/java/net/serg/SongServiceApplication.java
@@ -1,4 +1,5 @@
 package net.serg;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
