02a8e6c135c407f00ebfc2803f70a2a443b1f730
Sergey Pil <realsergeypil@gmail.com>
2024-02-27 15:45:18 +0600

    Make local and both eureka profiles work

--- resource-service/src/main/java/net/serg/resourceservice/config/RestTemplateConfiguration.java
+++ resource-service/src/main/java/net/serg/resourceservice/config/RestTemplateConfiguration.java
@@ -1,10 +1,9 @@
 package net.serg.resourceservice.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -12,7 +11,14 @@ public class RestTemplateConfiguration {
 
     @Bean
     @LoadBalanced
+    @Profile({"eureka-local", "eureka-compose"})
     public RestTemplate songServiceRestTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    @Profile("!eureka-local & !eureka-compose")
+    public RestTemplate eurekaSongServiceRestTemplate() {
         return new RestTemplate();
     }
 }
---
+++ song-service/src/main/java/net/serg/config/AppConfig.java
@@ -0,0 +1,11 @@
+package net.serg.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@EnableDiscoveryClient
+@Profile({"eureka-local", "eureka-compose"})
+public class AppConfig {
+}
